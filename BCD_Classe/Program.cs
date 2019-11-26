a0cccb8f281c812dd3b828a8dec0b49f1b453fe2
Memilnie <memilnie@gmail.com>
2019-11-26 21:51:42 +0100

    Add project files.

---
+++ BCD_Classe/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RPI;
+
+namespace BCD_Classe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
