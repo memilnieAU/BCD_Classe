788988938f3db2abbceb11b5de05dbe24c5afd88
Memilnie <memilnie@gmail.com>
2019-11-26 22:57:02 +0100

    Connection til Resp gennem class Kan udskrive specifik tal Kan  lave en hurtig bcd test

--- BCD_Classe/Program.cs
+++ BCD_Classe/Program.cs
@@ -9,8 +9,43 @@ namespace BCD_Classe
 {
     class Program
     {
+        private static RPi Resp;
+        
+      
         static void Main(string[] args)
         {
+           Resp = new RPi();
+
+            To7seg to7Seg = new To7seg(Resp);
+            
+
+            while (true)
+            {
+                Console.WriteLine("Tast et tal eller t(Test) eller q(Break)");
+                string input = Console.ReadLine() ;
+                if (input != "q" && input != "t")
+                {
+                    short tal = Convert.ToInt16(input);
+                    to7Seg.ShowNum(tal);
+                }
+                else if (input == "q")
+                {
+                    break;
+                }
+                else if (input == "t")
+                {
+                    to7Seg.Test();
+                }
+                
+                
+
+            
+            
+
+            }
+
+           
+        
         }
     }
 }
---
+++ BCD_Classe/To7seg.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RPI;
+using System.Threading;
+
+namespace BCD_Classe
+{
+    
+
+    
+
+    public class To7seg
+    {
+        private static RPi Resp;
+        private static RPI.Display.SevenSeg display;
+
+        public To7seg(RPi rpiref)
+        {
+            display = new RPI.Display.SevenSeg(rpiref);
+
+            display.Init_SevenSeg();
+            ShowNum(88);
+            display.Close_SevenSeg();
+            
+
+        }
+
+        public void Test()
+        {
+            
+
+            for (int i = 0; i < 99; i+=11)
+            {
+                int bcdResult = 0;
+                int shift = 0;
+                int testTal = 0;
+                testTal = i;
+                while (testTal > 0)
+                {
+                    
+                    bcdResult |= (testTal % 10) << (shift++ * 4);
+
+
+                    testTal /= 10;
+
+
+
+                }
+                display.Disp_SevenSeg((short)bcdResult);
+                Thread.Sleep(500);
+            }
+            display.Close_SevenSeg();
+
+
+        }
+
+   public void ShowNum(int pulse)
+        {
+            int bcdResult = 0;
+            int shift = 0;
+            while (pulse > 0)
+            {
+                int i = 0;
+                bcdResult |= (pulse % 10) << (shift++ * 4);
+
+
+                pulse /= 10;
+
+
+
+            }
+
+
+
+
+            //short sPulse = Convert.ToInt16(pulse);
+            display.Disp_SevenSeg((short)bcdResult);
+        }
+
+        public void Close()
+        {
+            display.Close_SevenSeg();
+        }
+
+
+
+
+    }
+}
