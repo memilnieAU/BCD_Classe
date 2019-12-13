0000ae6db9e332ee7ab58f257572dd98db50ec79
MemilnieAU <201908775@post.au.dk>
2019-12-13 14:37:43 +0100

    Knap add

---
+++ BCD_Classe/Knap.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RPI;
+
+namespace BCD_Classe
+{
+    public class Knap
+    {
+        public bool keyIsPressed = false;
+
+
+
+        private static Key startStop;
+
+        public Knap(RPi rpiref)
+        {
+            startStop = new RPI.Key(rpiref, Key.ID.P1);
+
+        }
+
+        public bool CheckKnap()
+        {
+            if (startStop.isPressed() == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+           
+        }
+
+
+    }
+}
--- BCD_Classe/Program.cs
+++ BCD_Classe/Program.cs
@@ -4,38 +4,92 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RPI;
+using System.Threading;
 
 namespace BCD_Classe
 {
     class Program
     {
         private static RPi Resp;
-        
-      
+        private static Key startStop;
+        private static RPI.Controller.PWM _pwm;
+
+        //Hej
+
         static void Main(string[] args)
         {
-           Resp = new RPi();
+            Resp = new RPi();
 
             To7seg to7Seg = new To7seg(Resp);
-
+            startStop = new RPI.Key(Resp, Key.ID.P1);
 
             to7Seg.HwTestMode(); //Denne kode kan skrives hvis man vil teste alle cifre
-            
+
             //Denne komando er den eneste der skal bruges hvis der skal skrives noget til Display
             //to7Seg.ShowNum(Convert.ToInt32(Console.ReadLine()));
             //to7Seg.Close();
-
+            //public bool keyIsPressed = false;
             
-
-
-            Console.ReadLine();
             
+            ///*
+                     
+
+            _pwm = new RPI.Controller.PWM(Resp);
+            _pwm.InitPWM();
+            Thread.Sleep(1000);
+            // Set dutycycle to 75%
+            Console.WriteLine("start med 75%");
+           _pwm.SetPWM(75);
+           
+            for (int i = 0; i <= 10; i++)
+            {
+                _pwm.SetPWM(10*i);
+                // Wait 2.5 seconds
+                Console.WriteLine(10*i+"%");
+                //Thread.Sleep(2500);
+                Console.ReadLine();
+            }
+            //*/
+
+            DateTime starttime = DateTime.Now;
+            Console.WriteLine("Start tid " + starttime);
+            DateTime stoptime = DateTime.Now;
+            TimeSpan deltatid = stoptime - starttime;
+            double delaytid = 5; //Den kan kun kører i 5 sekunder
+            while (deltatid.TotalSeconds <= delaytid) 
+            {
+                
+                _pwm.SetPWM(25);
+                Thread.Sleep(250);
+                _pwm.SetPWM(100);
+                Thread.Sleep(250);
+                stoptime = DateTime.Now;
+                Console.WriteLine("ny stoptid " + stoptime);
+                deltatid = stoptime - starttime;
+                Console.WriteLine("ny Delta " + deltatid);
 
 
+            }
+            _pwm.StopPWM();
+            while (true)
+            {
+                if (startStop.isPressed() == true)
+                {
+                    Console.WriteLine("True");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
 
 
 
 
         }
+
     }
 }
+
+
+
